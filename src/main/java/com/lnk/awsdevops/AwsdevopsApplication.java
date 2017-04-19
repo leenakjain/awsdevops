970a726a0739c1e93e45d3d0796f7fb816928e39
Leena Jain <leenakjain@gmail.com>
2017-04-19 23:12:23 +1000

    Initial Commit

---
+++ src/main/java/com/lnk/awsdevops/AwsdevopsApplication.java
@@ -0,0 +1,12 @@
+package com.lnk.awsdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsdevopsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lnk/awsdevops/AwsdevopsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lnk.awsdevops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsdevopsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
